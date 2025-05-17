51fd6506cdd23fef64aabbbfd4693a2ee82c34dc
Mark Petersen <mpetersen@lanl.gov>
2025-05-17 11:48:35 -0600

    title

--- app.py
+++ app.py
@@ -29,11 +29,14 @@ def adder_page():
         <html>
             <body>
                 {errors}
+                <h1>SoundSpel Translation Tool</h1>
+                <h2>Written as it Sounds. Pronounced as it's Written</h2>
                 <p>Enter your text here:</p>
                 <form method="post" action=".">
                     <p><textarea name="inputText" cols="60" rows="10" style="white-space:pre;"></textarea></p>
                     <p><input type="submit" value="translate into SoundSpel!" /></p>
                 </form>
+                <p>For more information, go to <a href="https://americanliteracy.com/">https://americanliteracy.com/</a></p>
             </body>
         </html>
     '''.format(errors=errors)
